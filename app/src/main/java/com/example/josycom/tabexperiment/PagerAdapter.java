6cfee5ed9b08883cfe033546fb119405990e0c92
MayorJay <joseolu4gsm@yahoo.com>
2019-08-24 11:04:38 +0100

    Initial commit

---
+++ app/src/main/java/com/example/josycom/tabexperiment/PagerAdapter.java
@@ -0,0 +1,35 @@
+package com.example.josycom.tabexperiment;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int numOfTabs) {
+        super(fm);
+        this.mNumOfTabs = numOfTabs;
+    }
+
+    /**
+     * Return the Fragment associated with a specified position
+     */
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0: return new TabFragment1();
+            case 1: return new TabFragment2();
+            case 2: return new TabFragment3();
+            default: return null;
+        }
+    }
+
+    /**
+     * Return the number of views available.
+     */
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
